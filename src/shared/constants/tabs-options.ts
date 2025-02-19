6d32d3f41c5f581c5ab0938416c2e2df44d524a3
tragedyfiftyone <148686852+tragedy51@users.noreply.github.com>
2025-02-19 20:33:11 +0500

    feat: label of buttons changed

--- src/shared/constants/tabs-options.ts
+++ src/shared/constants/tabs-options.ts
@@ -1,6 +1,6 @@
 import type { TabOption } from '../interfaces/TabOptions';
 
 export const tabsOptions: TabOption[] = [
-   { label: 'Распродажа', value: 'sale' },
-   { label: 'Дополнения для игр', value: 'dlc' }
+   { label: 'Игры', value: 'sale' },
+   { label: 'Дополнения', value: 'dlc' }
 ];
